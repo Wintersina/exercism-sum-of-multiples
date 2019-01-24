21e6121f7accc582f1c038869b9987e2f1a0ff46
Sina Serati <wintersina@gmail.com>
2019-01-24 09:59:14 -0800

    second run

--- src/main/scala/SumOfMultiples.scala
+++ src/main/scala/SumOfMultiples.scala
@@ -1,37 +1,13 @@
 import scala.collection.mutable.ListBuffer
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
-    var sum = 0
-    var range = new ListBuffer[Int]()
-    var f = new ListBuffer[Int]()
 
-    //make a range from 1 to limit
-    for(i <- 1 to limit-1){
-      range+= i
-    }
+    var range = 1 until limit
+    val sum = range.filter( i => factors.exists( i % _ == 0)).sum
 
-    range.map { x=>
-      if(!factors.isEmpty){
-        if( x % factors.toList(0) == 0) {
-          sum+=x
-        }
-        else if(factors.size > 1) {
-          if(x % factors.toList(1) == 0){
-            sum+=x
-          }
-          else if(factors.size > 2) {
-            if(x % factors.toList(2) == 0 ){
-              sum+=x
-            }
-          }
-        }
-        else{
-          sum
-        }
-      }
-    } 
-    
     sum
-    
   }
 }
+
+
+println(SumOfMultiples.sum(Set(3,5),10))
